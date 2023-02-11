d0f51dc6b73a2608060f6fab41da49c9637f9093
David.McGregor <David.McGregor@hl.co.uk>
2023-02-11 14:11:34 +0000

    initial commit

---
+++ src/main/java/com/example/MvRepTile3/MvRepTile3Application.java
@@ -0,0 +1,13 @@
+package com.example.MvRepTile3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvRepTile3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvRepTile3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MvRepTile3/MvRepTile3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MvRepTile3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvRepTile3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
