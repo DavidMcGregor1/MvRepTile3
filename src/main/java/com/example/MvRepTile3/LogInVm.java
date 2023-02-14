990988f68b2c217622ecc423ca4ea0a7981482e0
David McGregor <david@plisky.net>
2023-02-14 09:24:04 +0000

    test

---
+++ src/main/java/com/example/MvRepTile3/LogInVm.java
@@ -0,0 +1,20 @@
+package com.example.MvRepTile3;
+
+public class LogInVm {
+
+    public LogInVm(String theUsername, String thePassword) {
+        username = theUsername;
+        password = thePassword;
+
+    }
+
+    public LogInVm(int theId) {
+        id = theId;
+    }
+
+    public long id;
+    public String username;
+    public String password;
+    public boolean succeeded;
+
+}
---
+++ src/main/java/com/example/MvRepTile3/app/MainController.java
@@ -0,0 +1,63 @@
+package com.example.MvRepTile3.app;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import java.util.*;
+
+@Controller
+    public class MainController {
+        public MainController() {
+
+        }
+
+
+
+        @GetMapping("/newWorkout")
+            public String greeting() {
+                System.out.println("arrived at new workout page");
+                return "newWorkout";
+            }
+
+
+        @GetMapping("/login")
+            public String login() {
+                System.out.println("arrived at login page");
+                return "login";
+            }
+
+        @GetMapping("/home")
+            public String home() {
+                System.out.println("arrived at home page");
+                return "home";
+        }
+
+        @GetMapping("/viewWorkouts")
+            public String viewWorkouts() {
+                System.out.println("arrived at home page");
+                return "viewWorkouts";.
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
